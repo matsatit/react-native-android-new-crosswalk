332f5885fd13a71534c53675ad30205c98b63ad7
Hien Ton <matsatit@users.noreply.github.com>
2017-12-11 08:00:25 +0700

    Update get_library.js

--- get_library.js
+++ get_library.js
@@ -1,17 +1,21 @@
-'use strict';
+var version  = '22.52.561.4';
+var beta     = '';
 
-var version = '22.52.561.4';
-var beta    = '';
+var platform = require('os').platform();
+var exec     = require('child_process').execSync;
+var fs       = require('fs');
+var wget     = require('node-wget');
 
-var exec = require('child_process').exec;
-var fs   = require('fs');
-var wget = require('node-wget');
+fs.stat('./libs/xwalk_core_library-'+ version + '.aar',function(err){
+    if( err ){
+        downloadLibrary();
+    }
+})
 
 function prepareLibrary (filePath) {
     try {
         fs.unlinkSync('./libs/' + filePath);
-    }
-    catch (e) {
+    } catch (e) {
         console.log('No previous file');
     }
     fs.renameSync(filePath, './libs/' + filePath);
@@ -22,10 +26,9 @@ function handleDownloaded (error, data) {
     if (error) {
         console.error('Failed downloading file');
         console.error(error);
-    }
-    else {
-        console.log('File downloaded successfully');
+    } else {
         prepareLibrary(data.filepath);
+        modifyGuide()
     }
 }
 
@@ -35,4 +38,10 @@ function downloadLibrary () {
     wget(url, handleDownloaded);
 }
 
-downloadLibrary();
+function modifyGuide(){
+    console.log('\x1b[32m','\n Success download depedencies. \n')
+    console.log('\x1b[33m','For','\x1b[36m\x1b[1m','Linux-like OS','\x1b[33m','you can use')
+    console.log('\x1b[34m','cat ./node_modules/react-native-webkit-webview/modify_java_project_steps')
+    console.log('\x1b[33m','to show setps to modify project files.\n')
+    console.log('\x1b[33m','For Windows,you should open README.md for further modify info.\n')
+}
